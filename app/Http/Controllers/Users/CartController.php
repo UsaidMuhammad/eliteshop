c1642aac21d9c981ee3d5357b7ae70e191533373
UsaidMuhammad <ss4_usaid@hotmail.com>
2018-01-21 17:13:22 +0500

    add Cart controller

---
+++ app/Http/Controllers/Users/CartController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers\Users;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App;
+class CartController extends Controller
+{
+    public function cartindex()
+    {   
+        $data["cart"]=\Cart::instance('Shopping')->content();
+        return view('web.cart',$data);
+    }
+
+    public function cartadd()
+    {   
+        $id = \Request::get("_id");
+        $_type = \Request::get("_type");
+        $item = [];
+        
+        if ($_type == "men") {
+            $item = App\MenProducts::where("ProductID",$id)->get();
+            
+            if(count($item)!=0)
+            {
+                \Cart::instance('Shopping')->add($item[0]->ProductID, $item[0]->ProductName, \Request::get("quantity"), $item[0]->ProductPrice,['Image'=>\Request::get('_image')]);
+                return redirect('cart');
+            }
+            else
+            {
+                return redirect('404');
+            }
+        } else if ($_type == "women") {
+            $item = App\WomenProducts::where("ProductID",$id)->get();
+            if(count($item)!=0)
+            {
+                \Cart::instance('Shopping')->add($item[0]->ProductID, $item[0]->ProductName, \Request::get("quantity"), $item[0]->ProductPrice,['Image'=>\Request::get('_image')]);
+                return redirect('cart');
+            }
+            else
+            {
+                return redirect('404');
+            }
+        }
+    }
+
+    public function cartupdate()
+    {   
+        if (\Request::has("Delete")) {
+            \Cart::instance('Shopping')->remove(\Request::get('rowId'));
+        }
+
+        if (\Request::has("Update")) {
+            \Cart::instance('Shopping')->update(\Request::get('rowId'), \Request::get('quantity'));
+        }
+        return redirect('cart');
+        
+    }
+}
