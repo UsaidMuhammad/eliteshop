a86095cc6b09484691826e6bf990651edb5b2ea1
UsaidMuhammad <ss4_usaid@hotmail.com>
2018-01-23 17:29:29 +0500

    Users Pending order controller setup

--- app/Http/Controllers/Users/DashboardController.php
+++ app/Http/Controllers/Users/DashboardController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Users;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App;
 
 class DashboardController extends Controller
 {
@@ -19,7 +20,33 @@ class DashboardController extends Controller
 
     public function pendOrders()
     {
-        return view("users.dashboard.pending"); 
+        $data['item'] = App\Order::where([
+            ['UserID','=',\Session::get('UserID')],
+            ['Status','<','5'],
+        ])->get();
+       
+        $data['products'] = []; // will hold products data found via $data['array']
+        $data['array'] = []; // will hold arrays of my products data
+
+        for ($i=0; $i < count($data['item']); $i++) { 
+
+            $data['array'][] = unserialize($data['item'][$i]->ProductsArray);
+            
+            foreach ($data['array'][$i] as $key ) {
+            
+                if($key['_type']== 'men')
+                {
+                    $data['products'][$i][] = App\MenProducts::where('ProductID',$key['id'])->first();
+                }
+                else if($key['_type']=='women')
+                {
+                    $data['products'][$i][] = App\WomenProducts::where('ProductID',$key['id'])->first();
+                }
+            }
+        }
+
+        
+        return view("users.dashboard.pending", $data); 
     }
 
     public function info()
