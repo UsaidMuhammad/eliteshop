c5bf53630151f2ef86c3765a516d7d3f58e57991
UsaidMuhammad <ss4_usaid@hotmail.com>
2018-01-25 12:49:00 +0500

    removed search bar and social links from header

--- resources/views/web/includes/header.blade.php
+++ resources/views/web/includes/header.blade.php
@@ -32,16 +32,10 @@
 <!-- header-bot -->
 <div class="header-bot">
 	<div class="header-bot_inner_wthreeinfo_header_mid">
-		<div class="col-md-4 header-middle">
-			<form action="#" method="post">
-				<input type="search" name="search" placeholder="Search here..." required="">
-				<input type="submit" value=" ">
-				<div class="clearfix"></div>
-			</form>
-		</div>
+		
 		<!-- header-bot -->
-		<div class="col-md-4 logo_agile">
-			<h1>
+		<div class="col-md-4 logo_agile" style="margin:auto;width:100%;">
+			<h1 class="text-center">
 				<a href="{{url('/assets/web')}}/index.html">
 					<span>E</span>lite Shop
 					<i class="fa fa-shopping-bag top_logo_agile_bag" aria-hidden="true"></i>
@@ -50,50 +44,6 @@
 		</div>
 		<!-- header-bot -->
 		<div class="col-md-4 agileits-social top_content">
-			<ul class="social-nav model-3d-0 footer-social w3_agile_social">
-				<li class="share">Share On : </li>
-				<li>
-					<a href="#" class="facebook">
-						<div class="front">
-							<i class="fa fa-facebook" aria-hidden="true"></i>
-						</div>
-						<div class="back">
-							<i class="fa fa-facebook" aria-hidden="true"></i>
-						</div>
-					</a>
-				</li>
-				<li>
-					<a href="#" class="twitter">
-						<div class="front">
-							<i class="fa fa-twitter" aria-hidden="true"></i>
-						</div>
-						<div class="back">
-							<i class="fa fa-twitter" aria-hidden="true"></i>
-						</div>
-					</a>
-				</li>
-				<li>
-					<a href="#" class="instagram">
-						<div class="front">
-							<i class="fa fa-instagram" aria-hidden="true"></i>
-						</div>
-						<div class="back">
-							<i class="fa fa-instagram" aria-hidden="true"></i>
-						</div>
-					</a>
-				</li>
-				<li>
-					<a href="#" class="pinterest">
-						<div class="front">
-							<i class="fa fa-linkedin" aria-hidden="true"></i>
-						</div>
-						<div class="back">
-							<i class="fa fa-linkedin" aria-hidden="true"></i>
-						</div>
-					</a>
-				</li>
-			</ul>
-
 
 
 		</div>
